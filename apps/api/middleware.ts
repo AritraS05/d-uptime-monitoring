e4431bb4ac26ecce6b88c832fcdaffbab32e9983
aritra sarkar <aritrasarkar00007@gmail.com>
2025-03-29 14:11:25 +0000

    before val final

--- apps/api/middleware.ts
+++ apps/api/middleware.ts
@@ -2,19 +2,20 @@ import type { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { JWT_PUBLIC_KEY } from "./config";
 
-export function authMiddleware(req: Request, res: Response, next: NextFunction) {
+export const authMiddleware = (req: Request, res: Response, next: NextFunction): void => {
     const token = req.headers['authorization'];
     if (!token) {
-        return res.status(401).json({ error: 'Unauthorized' });
+        res.status(401).json({ error: 'Unauthorized' });
+        return;
     }
 
     const decoded = jwt.verify(token, JWT_PUBLIC_KEY);
     console.log(decoded);
     if (!decoded || !decoded.sub) {
-        return res.status(401).json({ error: 'Unauthorized' });
+        res.status(401).json({ error: 'Unauthorized' });
+        return;
     }
 
     req.userId = decoded.sub as string;
-    
-    next()
+    next();
 }
